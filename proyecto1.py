fe11a351718803b9b14a17c81af974c2676e2410
ricardojuarez <ricardojuarez1999@gmail.com>
2016-05-25 08:43:54 -0600

    cree un potenciador y casi creo los caracteres

--- proyecto1.py
+++ proyecto1.py
@@ -2,3 +2,12 @@ def prob_1(n):
 	return n %2 == 0
 def prob_2(n):
 	return (n-32)/1.8
+def prob_3(b,p):
+	b1 = b
+	for i in range(1,p):
+		b = b*b1
+	return b
+def prob_4(a,c):
+	lista = [a]
+	t = lista.split()
+	return t
